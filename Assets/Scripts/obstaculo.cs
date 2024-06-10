34da30ab33cc4d97cf0d3233b4aecb1f5a545973
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-10 11:33:54 -0300

    pontos

--- Assets/Scripts/obstaculo.cs
+++ Assets/Scripts/obstaculo.cs
@@ -10,6 +10,19 @@ public class obstaculo : MonoBehaviour
     [SerializeField]
     private float variacaoY;
 
+    [SerializeField]
+    private Vector3 posicaoPassaro;
+
+    private bool pontuei;
+
+    private UIcontroler controladorUI;
+
+    private void Start()
+    {
+        this.posicaoPassaro = GameObject.FindObjectOfType<passaro>().transform.position;
+        this.controladorUI = GameObject.FindObjectOfType<UIcontroler>();
+    }
+
     private void Awake()
     {
         this.transform.Translate(Vector3.up * Random.Range(-variacaoY,variacaoY));
@@ -18,6 +31,11 @@ public class obstaculo : MonoBehaviour
     void Update()
     {
         this.transform.Translate(Vector3.left * 0.6f * Time.deltaTime);
+        if (!this.pontuei && this.transform.position.x < posicaoPassaro.x)
+        {
+            this.controladorUI.adcPontos();
+            this.pontuei = true;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
